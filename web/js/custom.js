a8cc6960e95b77aece45ecf2d11e4ea6f7f114e8
Dieter Meys <dietermeys@gmail.com>
2019-12-16 21:11:46 +0100

    js voor meerdere personen

--- web/js/custom.js
+++ web/js/custom.js
@@ -3,14 +3,14 @@ $(window).on("load", function () {
     var rij =
         '<div class="form-row extraRij">' +
         '<label for="">Extra naam (aantalExtra)*</label>' +
-        '<input type="text" name="deelnemer[aantalExtra][naam][]" class="input" autocomplete="new" required>' +
+        '<input type="text" name="rsvp[personen][aantalExtra][naam]" id="rsvp_personen_aantalExtra_naam" class="input" autocomplete="new" required>' +
         '<label for="">Verzoeknummers</label>' +
-        '<input type="text" name="deelnemer[aantalExtra][verzoeknummer][]" class="input" autocomplete="off">' +
-        '<input type="text" name="deelnemer[aantalExtra][verzoeknummer][]" class="input" autocomplete="off">' +
+        '<input type="text" name="rsvp[personen][aantalExtra][nummers][0][nummer]" class="input" autocomplete="off">' +
+        '<input type="text" name="rsvp[personen][aantalExtra][nummers][1][nummer]" class="input" autocomplete="off">' +
         '<div class="bijsluiter">Welke 2 nummers wil je zeker horen op het feest?</div>' +
         '</div>';
 
-    $("#aantal").on("input", function () {
+    $(".aantalPersonen").on("input", function () {
         var aantalPersonen = $(this).val();
         var element = rij;
         var extra = '';
